2bf96ac2c708bb314c7457cf94e80f52d59869dd
dkause <d.g.kause@gmail.com>
2024-01-31 15:30:28 +0100

    Add movie and user schema to models.js and update login route in auth.js

--- index.js
+++ index.js
@@ -1,347 +1,56 @@
-const mongoose = require('mongoose')
-const Models = require('./models.js')
-const express = require('express')
-const { check, validationResult } = require('express-validator')
+// created by Copilot edited by dkause
 
-const Movies = Models.Movie
-const Users = Models.User
+/**
+ * Callback function for the error message.
+ *
+ * @param {Error} error - The error message.
+ * @param {boolean} result - The result of the callback.
+ * @returns {function} The callback function.
+ */
+return callback(new Error(message), false)
+// ...
+return callback(null, true)
 
-mongoose
-  .connect(
-    'mongodb+srv://dkause:ksOTsgxwTtF2X7sF@kausedb.qpgk51c.mongodb.net/myflix?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }
-  )
-  .then(() => {
-    console.log('kauseDB Connected')
-  })
-  .catch((error) => {
-    console.error('kauseDB connection Error: ' + error)
-  })
-
-  // mongoose.connect(
-  //   "mongodb://localhost:27017/myflix",
-  //   console.log("Mongoose Connected"),
-  //   {
-  //     useNewUrlParser: true,
-  //     useUnifiedTopology: true,
-  //   }
-  // );
-
-  ; (bodyParser = require('body-parser')), (uuid = require('uuid'))
-
-const app = express()
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-const cors = require('cors')
-
-// Allow only certain domains
-let allowedOrigins = [
-  'http://localhost:4200',
-  'http://localhost:8080',
-  'http://localhost:1234',
-  'http://testsite.com',
-  'https://movie-api-5rhq.onrender.com',
-  'https://kause-myflix.netlify.app',
-  'https://dkause.github.io',
-]
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true)
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin is not found on the list of allowed origins
-        let message =
-          'The CORS policy for this does not allow access from origin ' + origin
-        return callback(new Error(message), false)
-      }
-      return callback(null, true)
-    }
-  })
-)
-
-// import login from auth.js
+/**
+ * Import login from auth.js
+ * @type {function}
+ */
 let auth = require('./auth.js')(app)
+
+/**
+ * Passport module for authentication.
+ * @type {object}
+ */
 const passport = require('passport')
+
+/**
+ * Passport configuration.
+ */
 require('./passport.js')
 
-/* URL ENDPOINTS*/
-
-// respond with "hello world" when a GET request is made to the homepage
+/**
+ * Root route that sends a 'hello world' message.
+ * To spin up the free on render where the api is hosted.
+ * Use: https://movie-api-5rhq.onrender.com/
+ * Until the server responds no images are shown.
+ *
+ * @param {object} req - The request object.
+ * @param {object} res - The response object.
+ */
 app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-// Get a list of all Movies
-
+/**
+ * Route that gets a list of all movies.
+ *
+ * @param {object} req - The request object.
+ * @param {object} res - The response object.
+ */
 app.get(
   '/movies',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
     Movies.find()
-      .then((movies) => {
-        res.status(201).json(movies)
-      })
-      .catch((err) => {
-        console.error(err)
-        res.status(500).send('Error: ' + err)
-      })
   }
 )
-
-// Get data of a specific movie
-app.get(
-  '/movies/:Title',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    Movies.findOne({ Title: req.params.Title })
-      .then((movies) => {
-        res.json(movies)
-      })
-      .catch((err) => {
-        console.error(err)
-        res.status(500).send('Error: ' + err)
-      })
-  }
-)
-
-// Get data of a specific movie genre
-app.get(
-  '/movies/genre/:Genre',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    Movies.findOne({ 'Genre.Name': req.params.Genre })
-      .then((genre) => {
-        res.json(genre.Genre)
-        console.log(genre.Genre)
-      })
-      .catch((err) => {
-        console.error(err)
-        res.status(500).send('Error: ' + err)
-      })
-  }
-)
-
-// Get data of a specific Director
-app.get(
-  '/movies/director/:Name',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    Movies.findOne({ 'Director.Name': req.params.Name })
-      .then((directorName) => {
-        res.json(directorName.Director)
-        console.log(directorName)
-      })
-      .catch((err) => {
-        console.error(err)
-        res.status(500).send('Error: ' + err)
-      })
-  }
-)
-
-// Get a list of all Users
-app.get(
-  '/users',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    Users.find()
-      .then((users) => {
-        res.status(201).json(users)
-      })
-      .catch((err) => {
-        console.error(err)
-        res.status(500).send('Error: ' + err)
-      })
-  }
-)
-
-// Register/Create a user
-app.post(
-  '/users',
-  [
-    check('Username', 'Username is required').isLength({ min: 5 }),
-    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
-    check('Password', 'Password is required').not().isEmpty(),
-    check('Email', 'Email does not appear to be valid').isEmail()
-  ],
-  (req, res) => {
-    // check the validation object for errors
-
-    let errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() })
-    }
-    let hashedPassword = Users.hashPassword(req.body.Password)
-
-    Users.findOne({ Username: req.body.Username })
-      .then((user) => {
-        if (user) {
-          return res.status(400).send(req.body.Username + ' already exists')
-        } else {
-          Users.create({
-            Username: req.body.Username,
-            Password: hashedPassword,
-            Email: req.body.Email,
-            Birthday: req.body.Birthday
-          })
-            .then((user) => {
-              res.status(201).json(user)
-            })
-            .catch((error) => {
-              console.error(error)
-              res.status(500).send('Error: ' + error)
-            })
-        }
-      })
-      .catch((error) => {
-        console.error(error)
-        res.status(500).send('Error: ' + error)
-      })
-  }
-)
-
-// Get a user by username
-app.get(
-  '/users/:Username',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    Users.findOne({ Username: req.params.Username })
-      .then((user) => {
-        res.json(user)
-      })
-      .catch((err) => {
-        console.error(err)
-        res.status(500).send('Error: ' + err)
-      })
-  }
-)
-// TODO: validation for password
-// Update User by Name
-app.put(
-  '/users/:Username',
-  [
-    check('Username', 'Username is required').isLength({ min: 5 }),
-    check('Username', 'Username contains non-alphanumeric characters.').isAlphanumeric()
-  ],
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    let errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() })
-    }
-    let hashedPassword = Users.hashPassword(req.body.Password)
-
-    Users.findOneAndUpdate(
-      { Username: req.params.Username },
-      {
-        $set: {
-          Username: req.body.Username,
-          Password: hashedPassword,
-          Email: req.body.Email,
-          Birthday: req.body.Birthday
-        }
-      },
-      { new: true } // This line makes sure that the updated document is returned
-    )
-      .then((updatedUser) => {
-        res.status(200).json(updatedUser)
-      })
-      .catch((err) => {
-        console.error(err)
-        res.status(500).send('Error: ' + err)
-      })
-  }
-)
-
-// Delete User by Name
-app.delete(
-  '/users/:Username/:_id',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    Users.findOneAndRemove({ Username: req.params.Username })
-      .then((user) => {
-        if (!user) {
-          res.status(400).send(req.params.Username + ' was not found.')
-        } else {
-          res.status(200).send(req.params.Username + ' was deleted.')
-        }
-      })
-      .catch((err) => {
-        console.error(err)
-        res.status(500).send('Error: ' + err)
-      })
-  }
-)
-
-// Add a movie to favorites list
-app.post(
-  '/users/:Username/movies/:MovieId',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    Users.findOneAndUpdate(
-      { Username: req.params.Username },
-      { $push: { FavoriteMovies: req.params.MovieId } },
-      { new: true }
-    )
-      .then((updatedUser) => {
-        res.json(updatedUser)
-      })
-      .catch((err) => {
-        console.error(err)
-        res.status(500).send('Error: ' + err)
-      })
-  }
-)
-
-// Delete a movie from favorites list
-app.delete(
-  '/users/:Username/movies/:MovieId',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    Users.findOneAndUpdate(
-      { Username: req.params.Username },
-      { $pull: { FavoriteMovies: req.params.MovieId } },
-      { new: true }
-    )
-      .then((updatedUser) => {
-        res.json(updatedUser)
-        console.log(updatedUser)
-      })
-      .catch((err) => {
-        console.error(err)
-        res.status(500).send('Error: ' + err)
-      })
-  }
-)
-
-// Get a Movie Description by Name
-app.get(
-  '/movies/:Title',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    Movies.findOne({ Title: req.params.Title })
-      .then((movies) => {
-        res.json(movies)
-      })
-      .catch((err) => {
-        console.error(err)
-        res.status(500).send('Error: ' + err)
-      })
-  }
-)
-
-const port = process.env.PORT || 8080
-
-app.listen(port, '0.0.0.0', () => {
-  console.log('Your app is listening on port ' + port)
-})
-
-// Error Handler
-app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
-})
